2b5394faea6c49155125ead57c7943d2a6262e6b
Amin Mokhtari <amin.mokhtari94@gmail.com>
2020-11-25 02:56:36 +0330

    add attribute views

---
+++ resources/js/Pages/Attribute/AttributeEdit.vue
@@ -0,0 +1,95 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Edit Attribute {{ attribute.id }}
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                <jet-form-section @submitted="editAttribute">
+                    <template #title>
+                        Attribute
+                    </template>
+
+                    <template #description>
+                        Edit your attribute
+                    </template>
+
+                    <template #form>
+                        <!-- Name -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <jet-label for="name" value="Name" />
+                            <jet-input
+                                id="name"
+                                type="text"
+                                class="mt-1 block w-full"
+                                v-model="form.name"
+                                autocomplete="name"
+                            />
+                            <jet-input-error :message="form.error('name')" class="mt-2" />
+                        </div>
+                    </template>
+
+                    <template #actions>
+                        <jet-action-message :on="form.recentlySuccessful" class="mr-3">
+                            Saved.
+                        </jet-action-message>
+
+                        <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Save
+                        </jet-button>
+                    </template>
+                </jet-form-section>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout'
+import JetButton from '@/Jetstream/Button'
+import JetFormSection from '@/Jetstream/FormSection'
+import JetInput from '@/Jetstream/Input'
+import JetInputError from '@/Jetstream/InputError'
+import JetLabel from '@/Jetstream/Label'
+import JetActionMessage from '@/Jetstream/ActionMessage'
+
+export default {
+    components: {
+        AppLayout,
+        JetActionMessage,
+        JetButton,
+        JetFormSection,
+        JetInput,
+        JetInputError,
+        JetLabel
+    },
+
+    props: ['attribute'],
+
+    data() {
+        return {
+            form: this.$inertia.form(
+                {
+                    _method: 'PUT',
+                    name: this.attribute.name
+                },
+                {
+                    bag: 'editAttribute',
+                    resetOnSuccess: false
+                }
+            )
+        }
+    },
+
+    methods: {
+        editAttribute() {
+            this.form.post(route('attributes.update', this.attribute.id), {
+                preserveScroll: true
+            })
+        }
+    }
+}
+</script>
