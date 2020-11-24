5a754a925c09878a7638e5efad054c2f2eef518f
Amin Mokhtari <amin.mokhtari94@gmail.com>
2020-11-25 02:56:21 +0330

    update attribute controller

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -3,83 +3,104 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attribute;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AttributeController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        return Inertia::render('Attribute/AttributeList', [
+            'attributes' => Attribute::all()
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
-        //
+        return Inertia::render('Attribute/AttributeCreate');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        Attribute::create(['name' => $request->name]);
+
+        return redirect()->route('attributes.index')->with('message', 'Created Successfully.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Attribute  $attribute
-     * @return \Illuminate\Http\Response
+     * @param Attribute $attribute
+     * @return void
      */
     public function show(Attribute $attribute)
     {
-        //
+        // nothing to show
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Attribute  $attribute
-     * @return \Illuminate\Http\Response
+     * @param Attribute $attribute
+     * @return Response
      */
     public function edit(Attribute $attribute)
     {
-        //
+        return Inertia::render('Attribute/AttributeEdit', ['attribute' => $attribute]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Attribute  $attribute
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Attribute $attribute
+     * @return RedirectResponse
      */
     public function update(Request $request, Attribute $attribute)
     {
-        //
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        $attribute->update([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('attributes.index')->with('message', 'Updated Successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Attribute  $attribute
-     * @return \Illuminate\Http\Response
+     * @param Attribute $attribute
+     * @return RedirectResponse
+     * @throws \Exception
      */
     public function destroy(Attribute $attribute)
     {
-        //
+        $attribute->delete();
+        return redirect()->back()->with('message', 'Deleted Successfully.');
     }
 }
