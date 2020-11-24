a3709da274062a57dee7ce4f0283e0354843daf9
Amin Mokhtari <amin.mokhtari94@gmail.com>
2020-11-24 11:51:44 +0330

    create model attribute

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
+
+    Route::resource('products', 'ProductConrtoller');
 })->name('dashboard');
